99bc7cdaf4952138c42627928c62a4e8762c3d8a
Svein Gunnar Larsen <svein.gunnar.larsen@neptune-software.com>
2025-05-21 12:07:44 +0200

    Only WebApp

--- artifacts/Application/test/script/neptune.Script/Javascript.js
+++
@@ -1,4 +0,0 @@
-// How to Use:
-// 1.Ensure the ZXing library is included in the header of your application.
-// <script type="text/javascript" src="https://unpkg.com/@zxing/library@latest/umd/index.min.js"></script>
-console.log('test');
--- artifacts/Application/test_error/script/neptune.Typescript/Typescript.ts
+++
@@ -1,2 +0,0 @@
-asdfassdf.asdf.afadf.asd.f.
-<<<<>>>>
